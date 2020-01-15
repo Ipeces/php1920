0f4a0a020e23326ae1653dda941fcc52762bdcb7
Ipeces <59911292+Ipeces@users.noreply.github.com>
2020-01-15 09:09:49 +0100

    Add files via upload

---
+++ Bingo.php
@@ -0,0 +1,222 @@
+<HTML>
+<HEAD><TITLE> Bingo</TITLE></HEAD>
+<BODY>
+<?php
+
+$aacum11=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum11)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum11)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum11[$i]=$atle;
+	
+}
+$aacum21=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum21)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum21)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum21[$i]=$atle;
+	
+}
+$aacum31=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum31)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum31)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum31[$i]=$atle;
+	
+}
+$aacum41=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum41)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum41)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum41[$i]=$atle;
+	
+}
+$aacum51=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum51)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum51)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum51[$i]=$atle;
+	
+}
+$aacum61=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum61)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum61)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum61[$i]=$atle;
+	
+}
+$aacum71=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum71)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum71)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum71[$i]=$atle;
+	
+}
+$aacum81=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum81)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum81)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum81[$i]=$atle;
+	
+}
+$aacum91=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum91)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum91)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum91[$i]=$atle;
+	
+}
+$aacum101=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum101)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum101)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum101[$i]=$atle;
+	
+}
+$aacum111=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum111)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum111)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum111[$i]=$atle;
+	
+}
+$aacum121=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+for($i=0;$i<=15;$i++){
+ $atle=rand(1,60);
+	if (in_array($atle,$aacum121)){
+		$esta=true;
+		do{
+			if (!in_array($atle,$aacum121)){
+				$esta=false;
+			}
+			$atle=rand(1,60);
+		}while($esta==true);
+	}
+	 $aacum121[$i]=$atle;
+	
+}
+
+$carton1a1=$aacum11;
+
+$carton2a1=$aacum21;
+
+$carton3a1=$aacum31;
+
+$carton1a2=$aacum41;
+
+$carton2a2=$aacum51;
+
+$carton3a2=$aacum61;
+
+$carton1a3=$aacum71;
+
+$carton2a3=$aacum81;
+
+$carton3a3=$aacum91;
+
+$carton1a4=$aacum101;
+
+$carton2a4=$aacum111;
+
+$carton3a4=$aacum121;
+
+$jugador1=array($carton1a1,$carton2a1,$carton3a1);
+$jugador2=array($carton1a2,$carton2a2,$carton3a2);
+$jugador3=array($carton1a3,$carton2a3,$carton3a3);
+$jugador4=array($carton1a4,$carton2a4,$carton3a4);
+ echo "Jugadores";
+print_r(array_values($jugador1));echo "</br>";
+print_r(array_values($jugador2));echo "</br>";
+print_r(array_values($jugador3));echo "</br>";
+print_r(array_values($jugador4));echo "</br>";
+?>
+</BODY>
+</HTML>
